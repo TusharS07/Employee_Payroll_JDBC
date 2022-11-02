897e7db24df9d25963a1db72a7e20e4fb644a7b8
Salunkhe@07 <salunkhet07@gmail.com>
2022-11-02 18:43:31 +0530

    UC2_Retriving_Data_From_Database

--- src/main/java/com/bridgelabz/emp_payroll/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/emp_payroll/EmployeePayroll.java
@@ -2,12 +2,15 @@ package com.bridgelabz.emp_payroll;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class EmployeePayroll {
     public static void main(String[] args) {
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
         String userName = "root";
         String password = "Salunkhe@07";
+        Connection connection = null;
 
         //Driver loading
         try {
@@ -20,11 +23,26 @@ public class EmployeePayroll {
         //Connecting to database
         try {
             System.out.println("Connecting to database: " + jdbcURL);
-            Connection connection =  DriverManager.getConnection(jdbcURL, userName, password);
+            connection =  DriverManager.getConnection(jdbcURL, userName, password);
             System.out.println("Connection is successful!!!!! " + connection);
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println(" ");
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
+            while (resultSet.next()){
+                System.out.print(resultSet.getInt(1)+ " ");
+                System.out.print(resultSet.getString(2)+ " ");
+                System.out.print(resultSet.getString(3)+ " ");
+                System.out.print(resultSet.getDouble(4)+ " ");
+                System.out.print(resultSet.getDate(5)+ " ");
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
