4f425b0ebe3c625b4bc055d64e1b5c612c044ab5
Salunkhe@07 <salunkhet07@gmail.com>
2022-11-03 23:43:13 +0530

    UC5_SELECT_data_emp_joinDate_Data_Range

--- src/main/java/com/bridgelabz/emp_payroll/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/emp_payroll/EmployeePayroll.java
@@ -32,7 +32,7 @@ public class EmployeePayroll {
             String name = "Ashwini";
             double salary = 30000000;
             statement.executeUpdate("update employee_payroll set Salary = "+ salary +" where name = '"+ name +"'");
-            //show database
+            //show database data
             ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
             while (resultSet.next()){
                 System.out.print(resultSet.getInt(1)+ " ");
@@ -42,6 +42,19 @@ public class EmployeePayroll {
                 System.out.print(resultSet.getDate(5)+ " ");
                 System.out.println();
             }
+
+            //UC5= retrive employees who have joined in a particular data range from the payroll service database.
+            System.out.println("\n Employee data who joined from '2021-01-01' ");
+            ResultSet resultSet2 = statement.executeQuery("SELECT * FROM employee_payroll WHERE start BETWEEN CAST('2021-01-01' AS DATE) AND DATE(NOW())");
+            while (resultSet2.next()){
+                System.out.print(resultSet2.getInt(1)+ " ");
+                System.out.print(resultSet2.getString(2)+ " ");
+                System.out.print(resultSet2.getString(3)+ " ");
+                System.out.print(resultSet2.getDouble(4)+ " ");
+                System.out.print(resultSet2.getDate(5)+ " ");
+                System.out.println();
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
